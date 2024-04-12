b2b1435d901acb07c4dc24a6f2aa41cb537a4911
Shanmugamrskfamily <shanmugamrskfamily@email.com>
2024-04-12 14:32:01 +0530

    login counts model update

---
+++ Configuration/middleware.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const User=require('../Models/userModel');
+require('dotenv').config();
+
+const middleware = async (req, res, next) => {
+    const token = req.header('x-auth-token');
+    
+    if (!token) return res.status(403).json({ message: 'Access denied. No token provided.' });
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        // Fetch user role from the User model
+        const user = await User.findById(decoded.userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        req.user = { userRole:user.role }; 
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid token.' });
+    }
+};
+
+module.exports = { middleware };
--- Models/userModel.js
+++ Models/userModel.js
@@ -2,12 +2,15 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    role:{type: String, enum: ['Admin', 'User']},
+    role: { type: String, enum: ['Admin', 'User'] },
     password: { type: String, required: true },
     email: { type: String, required: true },
     gender: { type: String, enum: ['Male', 'Female', 'Transgender'], required: true },
-    count: { type: Number, default: 0 },
+    loginCounts: [{
+        loginDate: { type: Date, default: Date.now },
+        count: { type: Number, default: 0 }
+    }],
     lastLogin: { type: Date, default: Date.now }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
